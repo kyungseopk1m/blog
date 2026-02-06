74d8292900b10578ee4a762284b74bda7eafd5cb
kyungseopk1m <kks0919@kakao.com>
2026-02-07 03:06:34 +0900

    docs: import posts written until 2023

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const posts = defineCollection({
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
+    pubDate: z.coerce.date(),
     description: z.string(),
     draft: z.boolean().default(false),
     category: z.string().default('Uncategorized'),
